b51c1a64d37ca74efd67994dc49e9ffb02444d3d
Nizx <76437265+nizxcoder@users.noreply.github.com>
2023-02-09 23:25:22 +0530

    My last modified in this project, I hope no issue will be occurs in future

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 export default function Home(props){
     return(
-        <div className='container m-4' style={props.mode}>
-            <h1>Welcome to the home</h1>
+        <div className='container text-center' style={props.mode}>
+            <h1>Welcome To The Home </h1>
             <h3>This Website Can Manipulate Your Text As You Want.</h3>
+            <Link className='btn btn-outline-primary m-3' to="/textutils/textform">Click Here - To Manipulate</Link>
         </div>
     )
 }
